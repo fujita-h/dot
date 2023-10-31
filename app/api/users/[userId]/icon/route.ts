58faa0b0c3e5fa6aec8920de95dbd74536a7458c
Hiroshi Fujita <fujita-h@users.noreply.github.com>
2023-11-01 02:00:49 +0900

    update user icon api

--- app/api/users/[userId]/icon/route.ts
+++ app/api/users/[userId]/icon/route.ts
@@ -1,8 +1,8 @@
-import blobClient from '@/libs/azure/storeage-blob/instance';
 import { auth } from '@/libs/auth';
-import { getUserIdFromOid } from '@/libs/prisma/user-claim';
+import blobClient from '@/libs/azure/storeage-blob/instance';
+import { createDefaultUserIconSvg } from '@/libs/image/icon';
 import { checkUserExists } from '@/libs/prisma/user';
-import { unstable_cache } from 'next/cache';
+import { getUserIdFromOid } from '@/libs/prisma/user-claim';
 
 export async function GET(request: Request, { params }: { params: { userId: string } }) {
   if (!params.userId) {
@@ -30,58 +30,33 @@ export async function GET(request: Request, { params }: { params: { userId: stri
   }
 
   const { searchParams } = new URL(request.url);
-  const use_cache = searchParams.get('no-cache') === null ? true : false;
+  const no_cache = searchParams.get('no-cache') === null ? false : true;
+  const cache_control = no_cache ? 'no-store' : 'max-age=600';
 
-  if (use_cache) {
-    try {
-      const x = await getBlobWithCache('user', `${params.userId}/icon`);
-      if (x && x.buffer && x.contentType) {
-        return new Response(Buffer.from(x.buffer), { headers: [['Content-Type', x.contentType]] });
-      } else {
-        return new Response(null, { status: 404 });
-      }
-    } catch (error) {
-      console.log(error);
-      return new Response(null, { status: 500 });
-    }
-  } else {
-    try {
-      const blobResponse = await blobClient.download('user', `${params.userId}/icon`);
-      if (blobResponse.readableStreamBody && blobResponse.contentType) {
-        return new Response(nodeToWebStream(blobResponse.readableStreamBody), {
-          headers: [['Content-Type', blobResponse.contentType]],
-        });
-      } else {
-        return new Response(null, { status: 404 });
-      }
-    } catch (error) {
-      return new Response(null, { status: 500 });
-    }
-  }
-}
-
-const getBlobWithCache = unstable_cache(
-  async (containerName: string, blobName: string) => getBlob(containerName, blobName),
-  ['user-icons'],
-  {
-    revalidate: 60,
-    tags: ['user-icons'],
-  }
-);
-
-async function getBlob(containerName: string, blobName: string): Promise<{ contentType?: string; buffer: number[] }> {
-  return new Promise((resolve, reject) => {
-    return Promise.all([
-      blobClient.getProperties(containerName, blobName),
-      blobClient.downloadToBuffer(containerName, blobName),
-    ])
-      .then(([properties, buffer]) => {
-        resolve({ contentType: properties.contentType, buffer: buffer.toJSON().data });
-      })
-      .catch((error) => {
-        reject(error);
+  try {
+    const blobResponse = await blobClient.download('user', `${params.userId}/icon`);
+    if (blobResponse.readableStreamBody && blobResponse.contentType) {
+      return new Response(nodeToWebStream(blobResponse.readableStreamBody), {
+        headers: [
+          ['Content-Type', blobResponse.contentType],
+          ['Cache-Control', cache_control],
+        ],
       });
-  });
+    }
+    return new Response(null, { status: 500 });
+  } catch (error: any) {
+    if (error.statusCode === 404) {
+      const defaultUserIcon = createDefaultUserIconSvg(0, 360, 0, 100, 30, 80);
+      await blobClient.upload('user', `${params.userId}/icon`, 'image/svg+xml', defaultUserIcon);
+      return new Response(defaultUserIcon, {
+        headers: [
+          ['Content-Type', 'image/svg+xml'],
+          ['Cache-Control', cache_control],
+        ],
+      });
+    }
+    return new Response(null, { status: 500 });
+  }
 }
 
 function nodeToWebStream(nodeStream: NodeJS.ReadableStream) {
