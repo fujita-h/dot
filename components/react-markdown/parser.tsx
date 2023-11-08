642710a195b75f3c6ca5357832fcc66bfd4dd812
Hiroshi Fujita <fujita-h@users.noreply.github.com>
2023-11-09 02:04:25 +0900

    Add react-markdown parser and constants

---
+++ components/react-markdown/parser.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import rehypeRaw from 'rehype-raw';
+import rehypeSanitize, { defaultSchema } from 'rehype-sanitize';
+import { Schema } from 'hast-util-sanitize';
+import { CONTENT_ANCHOR_CLASS_NAME, CONTENT_ANCHOR_PREFIX } from '@/libs/constants';
+
+/**
+ * Parser of react-markdown.
+ * This component can use both client and server side.
+ */
+export function Parser({
+  children,
+  className,
+  addHeaderAnchor = false,
+}: {
+  children: string;
+  className?: string;
+  addHeaderAnchor?: boolean;
+}) {
+  const mySchema: Schema = { ...defaultSchema };
+
+  const H1 = ({ node, ...props }: any) => (
+    <h1 id={`${CONTENT_ANCHOR_PREFIX}-${node.position?.start.line.toString()}`} className={CONTENT_ANCHOR_CLASS_NAME}>
+      {props.children}
+    </h1>
+  );
+  const H2 = ({ node, ...props }: any) => (
+    <h2 id={`${CONTENT_ANCHOR_PREFIX}-${node.position?.start.line.toString()}`} className={CONTENT_ANCHOR_CLASS_NAME}>
+      {props.children}
+    </h2>
+  );
+
+  return (
+    <ReactMarkdown
+      className={className}
+      remarkPlugins={[remarkGfm]}
+      rehypePlugins={[rehypeRaw, [rehypeSanitize, mySchema]]}
+      unwrapDisallowed={false}
+      components={addHeaderAnchor ? { h1: H1, h2: H2 } : undefined}
+    >
+      {children}
+    </ReactMarkdown>
+  );
+}
---
+++ libs/constants.ts
@@ -0,0 +1,2 @@
+export const CONTENT_ANCHOR_PREFIX = 'content-line';
+export const CONTENT_ANCHOR_CLASS_NAME = 'content-lines';
