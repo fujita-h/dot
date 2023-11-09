58e6e119f6fe66868284a15f3d4cd60ba9598c95
Hiroshi Fujita <fujita-h@users.noreply.github.com>
2023-11-09 23:16:01 +0900

    Add function to fetch note with user, group, and topics

--- libs/prisma/note.ts
+++ libs/prisma/note.ts
@@ -2,10 +2,15 @@
 
 import prisma from '@/libs/prisma/instance';
 
-export function getNote(noteId: string) {
+export function getNoteWithUserGroupTopics(noteId: string) {
   return prisma.note
     .findUnique({
       where: { id: noteId },
+      include: {
+        User: true,
+        Group: true,
+        Topics: { include: { Topic: true } },
+      },
     })
     .catch((e) => {
       console.error(e);
