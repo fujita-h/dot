5b1114945397ff9328cef4e36d99c10b09587013
Hiroshi Fujita <fujita-h@users.noreply.github.com>
2023-11-10 23:51:01 +0900

    Add stock button component and related functions

--- libs/constants.ts
+++ libs/constants.ts
@@ -1,2 +1,4 @@
 export const CONTENT_ANCHOR_PREFIX = 'content-line';
 export const CONTENT_ANCHOR_CLASS_NAME = 'content-lines';
+
+export const DEFAULT_STOCK_LABEL_NAME = 'あとで読む';
---
+++ libs/prisma/stock.ts
@@ -0,0 +1,41 @@
+'server-only';
+
+import prisma from '@/libs/prisma/instance';
+
+export function getStockedLabels(userId: string, noteId: string) {
+  return prisma.stock
+    .findMany({
+      where: { userId, noteId },
+      include: { Label: true },
+    })
+    .catch((e) => {
+      console.error(e);
+      throw new Error('Error occurred while fetching userStockedNote');
+    });
+}
+
+export function getLabels(userId: string) {
+  return prisma.stockLabel
+    .findMany({
+      where: { userId },
+      orderBy: [{ default: 'desc' }, { name: 'asc' }],
+    })
+    .catch((e) => {
+      console.error(e);
+      throw new Error('Error occurred while fetching userStockedNote');
+    });
+}
+
+export function getStockedUsersCount(noteId: string) {
+  return prisma.stock
+    .findMany({
+      select: { userId: true },
+      where: { noteId },
+      distinct: ['userId'],
+    })
+    .then((res) => res.length)
+    .catch((e) => {
+      console.error(e);
+      throw new Error('Error occurred while fetching userStockedNote');
+    });
+}
