48f407b51a27c12cdda533d9735e80e55deced11
Hiroshi Fujita <fujita-h@users.noreply.github.com>
2023-11-15 01:11:03 +0900

    Add getNotesWithUserGroupTopics function to fetch notes with user group topics

--- libs/prisma/note.ts
+++ libs/prisma/note.ts
@@ -24,3 +24,28 @@ export function getNoteWithUserGroupTopics(noteId: string, requestUserId: string
       throw new Error('Error occurred while fetching note');
     });
 }
+
+export function getNotesWithUserGroupTopics(requestUserId: string, take?: number, skip?: number) {
+  return prisma.note
+    .findMany({
+      where: {
+        OR: [
+          { Group: null },
+          { Group: { type: 'PUBLIC' } },
+          { Group: { type: 'PRIVATE', Members: { some: { userId: requestUserId } } } },
+        ],
+      },
+      orderBy: { releasedAt: 'desc' },
+      take,
+      skip,
+      include: {
+        User: true,
+        Group: true,
+        Topics: { include: { Topic: true } },
+      },
+    })
+    .catch((e) => {
+      console.error(e);
+      throw new Error('Error occurred while fetching notes');
+    });
+}
