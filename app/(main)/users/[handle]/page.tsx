03626a90ba646d631adf694670f10dc8c1620e7f
Hiroshi Fujita <fujita-h@users.noreply.github.com>
2023-11-15 23:59:40 +0900

    Add user icon and adjust user info layout

--- app/(main)/users/[handle]/page.tsx
+++ app/(main)/users/[handle]/page.tsx
@@ -7,6 +7,7 @@ import { getUserIdFromSession } from '@/libs/auth/utils';
 import { SITE_NAME } from '@/libs/constants';
 import { getNotesWithUserGroupTopics } from '@/libs/prisma/note';
 import { getUserFromHandle } from '@/libs/prisma/user';
+import clsx from 'clsx';
 import { Metadata } from 'next';
 
 type Props = {
@@ -41,19 +42,34 @@ export default async function Page({ params }: Props) {
   return (
     <div className="space-y-4">
       <div className="bg-white rounded-md">
-        <div className="bg-white relative w-full h-[80px] lg:h-[160px]">
+        <div
+          className={clsx('bg-white relative w-full', 'h-[80px] sm:h-[100px] md:h-[120px] lg:h-[140px] xl:h-[160px]')}
+        >
           <img
             src={`/api/users/${user.id}/image`}
             className="absolute top-0 w-full h-full object-cover"
             alt="user image"
           />
-          <div className="absolute top-[50%] left-[5%] w-[80px] h-[80px] lg:w-[160px] lg:h-[160px] bg-white rounded-full">
-            <img src={'/api/user/icon'} className="w-full h-full p-2 rounded-full" alt="user-icon" />
+          <div
+            className={clsx(
+              'absolute bg-white rounded-full',
+              'top-[60%] sm:top-[50%] left-[5%]',
+              'w-[80px] sm:w-[100px] md:w-[120px] lg:w-[140px] xl:w-[160px]',
+              'h-[80px] sm:h-[100px] md:h-[120px] lg:h-[140px] xl:h-[160px]'
+            )}
+          >
+            <img src={'/api/user/icon'} className="w-full h-full p-1 lg:p-2 rounded-full" alt="user-icon" />
           </div>
         </div>
-        <div className="mt-1 lg:mt-3 ml-[calc(5%_+_80px)] lg:ml-[calc(5%_+_160px)] min-h-[60px] lg:min-h-[80px]">
-          <div className="text-xl lg:text-3xl font-bold truncate">{user.name}</div>
-          <div className="text-sm lg:text-base text-gray-500 truncate">@{user.handle}</div>
+        <div
+          className={clsx(
+            'mt-1 lg:mt-2',
+            'ml-[calc(5%_+_80px_+_5px)] sm:ml-[calc(5%_+_100px_+_5px)] md:ml-[calc(5%_+_120px_+_5px)] lg:ml-[calc(5%_+_140px_+_5px)] xl:ml-[calc(5%_+_160px_+_5px)]',
+            'min-h-[60px] md:min-h-[70px] lg:min-h-[80px] xl:min-h-[90px]'
+          )}
+        >
+          <div className="text-base md:text-xl lg:text-2xl xl:text-3xl font-bold truncate">{user.name}</div>
+          <div className="text-xs md:text-sm lg:text-base text-gray-500 truncate">@{user.handle}</div>
         </div>
       </div>
       <div className="md:flex md:gap-1">
