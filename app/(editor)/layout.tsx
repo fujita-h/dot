8233304d6d28e329a27896e7a724b4bd964b543a
Hiroshi Fujita <fujita-h@users.noreply.github.com>
2023-11-18 14:45:26 +0900

    Refactor layout component to include a white background and a max-width container

--- app/(editor)/layout.tsx
+++ app/(editor)/layout.tsx
@@ -8,7 +8,9 @@ export default async function Layout({ children }: { children: React.ReactNode }
   return (
     <div className="min-h-screen h-screen bg-slate-100">
       <header>
-        <Navbar userName={userName} email={email} />
+        <div className="bg-white">
+          <Navbar userName={userName} email={email} />
+        </div>
       </header>
       <main className="relative z-[1] h-[calc(100%_-_64px)]">{children}</main>
     </div>
--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -9,7 +9,11 @@ export default async function Layout({ children }: { children: React.ReactNode }
   return (
     <div className="min-h-screen bg-slate-100">
       <header>
-        <Navbar userName={userName} email={email} />
+        <div className="bg-white">
+          <div className="max-w-screen-2xl mx-auto">
+            <Navbar userName={userName} email={email} />
+          </div>
+        </div>
       </header>
       <main className="relative z-[1]">{children}</main>
       <footer className="sticky top-[100vh]">
