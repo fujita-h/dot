ce40aa52aae3822787b5fdf80417040966cb9f6e
Hiroshi Fujita <fujita-h@users.noreply.github.com>
2023-11-21 01:51:22 +0900

    Refactor stack-list component

--- components/notes/stack-list.tsx
+++ components/notes/stack-list.tsx
@@ -9,45 +9,48 @@ type Note = {
     handle: string;
     name: string;
   };
+  Group: {
+    id: string;
+    handle: string;
+    name: string;
+  } | null;
 };
 
 export function StackList({ notes }: { notes: Note[] }) {
   return (
     <ul role="list" className="divide-y divide-gray-200">
       {notes.map((note) => (
-        <li
-          key={note.id}
-          className="relative flex flex-wrap items-center justify-between gap-x-6 gap-y-4 py-5 sm:flex-nowrap"
-        >
+        <li key={note.id} className="relative px-4 py-5">
           <div>
             <div className="flex gap-2">
-              <div className="z-[2]">
+              <div className="flex-none z-[1]">
                 <Link href={`/users/${note.User.handle}`} className="group">
                   <img
                     src={`/api/users/${note.User.id}/icon`}
-                    className="w-10 h-10 rounded-full group-hover:opacity-80"
+                    className="w-9 h-9 rounded-full group-hover:opacity-80"
                     alt="user image"
                   />
                 </Link>
               </div>
-              <div className="flex flex-col">
-                <div className="z-[2]">
-                  <Link href={`/users/${note.User.handle}`} className="hover:underline">
-                    <p className="text-sm text-gray-800">
+              <div className="flex-1 flex flex-col">
+                <div className="z-[1]">
+                  <p className="text-sm text-gray-800">
+                    <Link href={`/users/${note.User.handle}`} className="hover:underline">
                       <span>@{note.User.handle}</span>
                       {note.User.name && <span className="ml-1">({note.User.name})</span>}
-                    </p>
-                  </Link>
+                    </Link>
+                  </p>
                 </div>
                 <div>
                   <p className="text-sm text-gray-800">{note.releasedAt.toLocaleDateString('ja-jp')}</p>
                 </div>
               </div>
+              <div className="flex-none">right</div>
             </div>
             <div className="ml-12 mt-2">
               <Link href={`/notes/${note.id}`} className="group">
                 <span className="absolute inset-x-0 -top-px bottom-0" />
-                <p className="text-xl font-medium font-noto-sans-jp truncate text-gray-600 group-hover:underline">
+                <p className="text-xl font-medium font-noto-sans-jp truncate text-gray-700 group-hover:underline">
                   {note.title || 'タイトルなし'}
                 </p>
               </Link>
