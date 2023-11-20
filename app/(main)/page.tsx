f342256a646c99c3bffe4f6a59fb11b272918ae9
Hiroshi Fujita <fujita-h@users.noreply.github.com>
2023-11-21 02:37:30 +0900

    Refactor page component to handle authentication and display error pages

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,7 +1,24 @@
-import prisma from '@/libs/prisma/instance';
+import { SignInForm } from '@/components/auth/sign-in-form';
+import { Error404, Error500 } from '@/components/error';
 import { auth } from '@/libs/auth';
+import { getUserIdFromSession } from '@/libs/auth/utils';
+
 export default async function Page() {
   const session = await auth();
+  const { status, userId, error } = await getUserIdFromSession(session, true);
+  if (status === 401) return <SignInForm />;
+  if (status === 500) return <Error500 />;
+  if (status === 404 || !userId) return <Error404 />;
 
-  return <>home page</>;
+  return (
+    <div className="print:bg-white">
+      <div className="max-w-screen-2xl mx-auto">
+        <div className="p-2 md:p-4">
+          <main>
+            <div>Home</div>
+          </main>
+        </div>
+      </div>
+    </div>
+  );
 }
