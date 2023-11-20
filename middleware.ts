6f972228c588e5a1569f2313dc535e4165e16a56
Hiroshi Fujita <fujita-h@users.noreply.github.com>
2023-11-20 19:32:22 +0900

    Update config to exclude signin page from middleware

--- middleware.ts
+++ middleware.ts
@@ -15,7 +15,8 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
+     * - signin (signin page)
      */
-    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico|signin).*)',
   ],
 };
