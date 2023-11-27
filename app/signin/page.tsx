452abcf776a0698e29b420c9a9532f1d29fbaf00
Hiroshi Fujita <fujita-h@users.noreply.github.com>
2023-11-27 21:48:33 +0900

    Add auto sign-in functionality to the sign-in page

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,5 +1,13 @@
+import { AutoSignInForm } from '@/components/auth/auto-sign-in-form';
 import { SignInForm } from '@/components/auth/sign-in-form';
+import { auth } from '@/libs/auth';
+import { getUserIdFromSession } from '@/libs/auth/utils';
 
-export default function Page() {
-  return <SignInForm />;
+export default async function Page() {
+  const session = await auth();
+  const { userId } = await getUserIdFromSession(session);
+  if (!userId) {
+    return <SignInForm />;
+  }
+  return <AutoSignInForm callbackUrl="/" />;
 }
---
+++ components/auth/auto-sign-in-form.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import { TextLogo } from '@/components/text-logo';
+import { BuiltInProviderType } from 'next-auth/providers';
+import { LiteralUnion, signIn } from 'next-auth/react';
+import { useEffect } from 'react';
+
+export function AutoSignInForm({
+  provider = 'azure-ad',
+  callbackUrl,
+}: {
+  provider?: LiteralUnion<BuiltInProviderType>;
+  callbackUrl?: string;
+}) {
+  useEffect(() => {
+    signIn(provider, { callbackUrl });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div className="fixed inset-0 bg-gray-300 flex justify-center items-center">
+      <div className="bg-white rounded-md shadow-sm p-4 w-96">
+        <div className="text-center">
+          <div>
+            <TextLogo />
+          </div>
+          <div>Sign in to your account</div>
+          <div className="mt-6">
+            <div className="bg-indigo-700 text-white p-2 rounded-md w-full">
+              <div className="flex gap-3 items-center justify-center">
+                <svg
+                  className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                  <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                  ></path>
+                </svg>
+                <div>Please wait..</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
