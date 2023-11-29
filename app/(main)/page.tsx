556b82d9ad9be9d110eddf0fcecfc45fbc1f59b2
Hiroshi Fujita <fujita-h@users.noreply.github.com>
2023-11-30 01:36:49 +0900

    Add FollowingTopics component to main page

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,10 +1,9 @@
 import { SignInForm } from '@/components/auth/sign-in-form';
 import { Error404, Error500 } from '@/components/error';
 import { TrendingNotes } from '@/components/notes/trending';
+import { FollowingTopics } from '@/components/topics/following';
 import { auth } from '@/libs/auth';
 import { getUserIdFromSession } from '@/libs/auth/utils';
-import { getFollowingTopicsByUserId } from '@/libs/prisma/topic';
-import Link from 'next/link';
 
 export default async function Page() {
   const session = await auth();
@@ -13,8 +12,6 @@ export default async function Page() {
   if (status === 500) return <Error500 />;
   if (status === 404 || !userId) return <Error404 />;
 
-  const topics = await getFollowingTopicsByUserId(userId).catch((e) => []);
-
   return (
     <div className="print:bg-white">
       <div className="max-w-screen-2xl mx-auto">
@@ -22,29 +19,12 @@ export default async function Page() {
           <main>
             <div className="md:flex md:gap-1">
               <div className="flex-none w-80 p-2">
-                {topics.length > 0 && (
-                  <div className="mx-2">
-                    <h3 className="text-sm font-noto-sans-jp font-semibold">フォロー中のトピック</h3>
-                    <div className="mt-1 mx-1 flex flex-col gap-0.5">
-                      {topics.map((topic) => (
-                        <div key={topic.id}>
-                          <Link key={topic.id} href={`/topics/${topic.handle}`}>
-                            <div className="flex items-center gap-3 px-3 py-1 hover:bg-white rounded-sm">
-                              <div className="flex-shrink-0">
-                                <img
-                                  src={`/api/topics/${topic.id}/icon`}
-                                  className="w-4 h-4 rounded-full"
-                                  alt="topic icon"
-                                />
-                              </div>
-                              <div className="flex-none text-sm text-gray-900 truncate">{topic.name}</div>
-                            </div>
-                          </Link>
-                        </div>
-                      ))}
-                    </div>
+                <div className="mx-2">
+                  <h3 className="text-sm font-noto-sans-jp font-semibold">フォロー中のトピック</h3>
+                  <div className="mt-1 mx-1 flex flex-col gap-0.5">
+                    <FollowingTopics userId={userId} />
                   </div>
-                )}
+                </div>
               </div>
               <div className="flex-1">
                 <div className="flex flex-col gap-3">
