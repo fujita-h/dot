1d231d4214f1df99e72b40af2287c2c8d8e948ee
Hiroshi Fujita <fujita-h@users.noreply.github.com>
2023-11-30 01:26:12 +0900

    Refactor stack-list component to display user and group information

--- components/notes/stack-list.tsx
+++ components/notes/stack-list.tsx
@@ -22,30 +22,61 @@ export function StackList({ notes }: { notes: Note[] }) {
       {notes.map((note) => (
         <li key={note.id} className="relative px-4 py-5">
           <div>
-            <div className="flex gap-2">
+            <div className="flex gap-3">
               <div className="flex-none z-[1]">
-                <Link href={`/users/${note.User.handle}`} className="group">
-                  <img
-                    src={`/api/users/${note.User.id}/icon`}
-                    className="w-9 h-9 rounded-full group-hover:opacity-80"
-                    alt="user image"
-                  />
-                </Link>
+                {note.Group ? (
+                  <div className="relative">
+                    <Link href={`/groups/${note.Group.handle}`} className="group">
+                      <img
+                        src={`/api/groups/${note.Group.id}/icon`}
+                        className="w-10 h-10 rounded-md group-hover:opacity-80"
+                        alt="group image"
+                      />
+                    </Link>
+                    <div className="absolute top-6 left-6">
+                      <div className="bg-white rounded-full w-5 h-5 p-[1px] flex justify-center items-center">
+                        <Link href={`/users/${note.User.handle}`} className="group">
+                          <img
+                            src={`/api/users/${note.User.id}/icon`}
+                            className="w-full h-full rounded-full group-hover:opacity-80"
+                            alt="user image"
+                          />
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                ) : (
+                  <Link href={`/users/${note.User.handle}`} className="group">
+                    <img
+                      src={`/api/users/${note.User.id}/icon`}
+                      className="w-10 h-10 rounded-full group-hover:opacity-80"
+                      alt="user image"
+                    />
+                  </Link>
+                )}
               </div>
               <div className="flex-1 flex flex-col">
+                <div>
+                  <p className="text-sm text-gray-800">{note.releasedAt.toLocaleDateString('ja-jp')}</p>
+                </div>
                 <div className="z-[1]">
                   <p className="text-sm text-gray-800">
                     <Link href={`/users/${note.User.handle}`} className="hover:underline">
                       <span>@{note.User.handle}</span>
                       {note.User.name && <span className="ml-1">({note.User.name})</span>}
                     </Link>
+                    {note.Group && (
+                      <span>
+                        <span> in </span>
+                        <Link href={`/groups/${note.Group.handle}`} className="hover:underline">
+                          {note.Group.name && <span>{note.Group.name}</span>}
+                        </Link>
+                      </span>
+                    )}
                   </p>
                 </div>
-                <div>
-                  <p className="text-sm text-gray-800">{note.releasedAt.toLocaleDateString('ja-jp')}</p>
-                </div>
               </div>
-              <div className="flex-none">right</div>
+              <div className="flex-none">{/* Right side */}</div>
             </div>
             <div className="ml-12 mt-2">
               <Link href={`/notes/${note.id}`} className="group">
