f36d6db243f087e5acb9b4f8e0a4f8f80e868c36
Hiroshi Fujita <fujita-h@users.noreply.github.com>
2023-11-30 01:37:55 +0900

    Add FollowingTopics component to timeline page

--- app/(main)/timeline/page.tsx
+++ app/(main)/timeline/page.tsx
@@ -1,11 +1,12 @@
 import { SignInForm } from '@/components/auth/sign-in-form';
 import { Error404, Error500 } from '@/components/error';
+import { StackList } from '@/components/notes/stack-list';
+import { FollowingTopics } from '@/components/topics/following';
 import { auth } from '@/libs/auth';
 import { getUserIdFromSession } from '@/libs/auth/utils';
-import { getTimelineNotesWithUserGroupTopics } from '@/libs/prisma/note';
-import { StackList } from '@/components/notes/stack-list';
-import { Metadata } from 'next';
 import { SITE_NAME } from '@/libs/constants';
+import { getTimelineNotesWithUserGroupTopics } from '@/libs/prisma/note';
+import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: `タイムライン - ${SITE_NAME}`,
@@ -23,7 +24,14 @@ export default async function Page() {
   return (
     <div>
       <div className="md:flex md:gap-1">
-        <div className="flex-none w-80 p-2">left menu</div>
+        <div className="flex-none w-80 p-2">
+          <div className="mx-2">
+            <h3 className="text-sm font-noto-sans-jp font-semibold">フォロー中のトピック</h3>
+            <div className="mt-1 mx-1 flex flex-col gap-0.5">
+              <FollowingTopics userId={userId} />
+            </div>
+          </div>
+        </div>
         <div className="flex-1">
           <div className="flex flex-col gap-3">
             <div className="bg-white rounded-md p-2">
