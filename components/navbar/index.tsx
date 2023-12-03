7b01c67c61b4c265bb0987fa885bc23066e273fe
Hiroshi Fujita <fujita-h@users.noreply.github.com>
2023-12-04 00:42:51 +0900

    Temporary hide notification icon

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -91,13 +91,13 @@ export function Navbar({ userName, groups }: { userName: string; groups: { id: s
                         </form>
                       </div>
                     </div>
-                    <button
+                    {/* <button
                       type="button"
                       className="flex-shrink-0 rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                     >
                       <span className="sr-only">View notifications</span>
                       <BellIcon className="h-7 w-7" aria-hidden="true" />
-                    </button>
+                    </button> */}
                     {/* Profile dropdown */}
                     <Menu as="div" className="ml-1 relative flex-shrink-0">
                       <div>
@@ -266,13 +266,13 @@ export function Navbar({ userName, groups }: { userName: string; groups: { id: s
                         <span className="sr-only">Search</span>
                         <MagnifyingGlassIcon className="h-6 w-6" aria-hidden="true" />
                       </Link>
-                      <button
+                      {/* <button
                         type="button"
                         className="rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                       >
                         <span className="sr-only">View notifications</span>
                         <BellIcon className="h-6 w-6" aria-hidden="true" />
-                      </button>
+                      </button> */}
                     </div>
                   </div>
                   <div className="mt-3 space-y-1">
