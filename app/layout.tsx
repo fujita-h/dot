3e03d1237f7f691586e3aa6f28f2db61e5bf88bd
Hiroshi Fujita <fujita-h@users.noreply.github.com>
2023-12-10 01:10:46 +0900

    Add overflow-y-scroll class to html element

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="ja">
+    <html lang="ja" className="overflow-y-scroll">
       <body
         className={clsx(
           // default fonts
