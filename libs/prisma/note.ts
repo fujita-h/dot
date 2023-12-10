fe764ad163569e0fe547d849c75398372f641a37
Hiroshi Fujita <fujita-h@users.noreply.github.com>
2023-12-11 01:09:51 +0900

    Add support for retrieving commented notes

--- libs/prisma/note.ts
+++ libs/prisma/note.ts
@@ -51,6 +51,32 @@ export function getNotesWithUserGroupTopics(requestUserId: string, take?: number
     });
 }
 
+export function getCommentedNotesWithUserGroupTopics(requestUserId: string, take?: number, skip?: number) {
+  return prisma.note
+    .findMany({
+      where: {
+        Comments: { some: { userId: requestUserId } },
+        OR: [
+          { Group: null },
+          { Group: { type: 'PUBLIC' } },
+          { Group: { type: 'PRIVATE', Members: { some: { userId: requestUserId } } } },
+        ],
+      },
+      orderBy: { releasedAt: 'desc' }, // ToDo: Sort by user's latest comment date
+      take,
+      skip,
+      include: {
+        User: true,
+        Group: true,
+        Topics: { include: { Topic: true } },
+      },
+    })
+    .catch((e) => {
+      console.error(e);
+      throw new Error('Error occurred while fetching notes');
+    });
+}
+
 export function getNotesWithUserGroupTopicsByGroupId(
   groupId: string,
   requestUserId: string,
