e5e4711138f5026ecdae666d59bd0252342e28ee
Hiroshi Fujita <fujita-h@users.noreply.github.com>
2023-12-11 20:08:11 +0900

    Fix layout component to remove class

--- app/(editor)/layout.tsx
+++ app/(editor)/layout.tsx
@@ -1,3 +1,3 @@
 export default async function Layout({ children }: { children: React.ReactNode }) {
-  return <div className="min-h-screen h-screen bg-slate-100">{children}</div>;
+  return <div className="min-h-screen bg-slate-100">{children}</div>;
 }
