8dd4124fea5627ef6fac6adfbaf8fd79ae919771
Hiroshi Fujita <fujita-h@users.noreply.github.com>
2023-12-14 00:16:55 +0900

    Add source code CSS

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import clsx from 'clsx';
 import type { Metadata } from 'next';
 import { SITE_NAME } from '@/libs/constants';
-import { Inter, Noto_Sans_JP } from 'next/font/google';
+import { Inter, Noto_Sans_JP, Source_Code_Pro } from 'next/font/google';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 const notoSansJP = Noto_Sans_JP({ subsets: ['latin'], variable: '--font-noto-sans-jp' });
+const sourceCodePro = Source_Code_Pro({ subsets: ['latin'], variable: '--font-source-code-pro' });
 
 export const metadata: Metadata = {
   title: SITE_NAME,
@@ -20,7 +21,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           inter.className,
           // optional fonts
           inter.variable,
-          notoSansJP.variable
+          notoSansJP.variable,
+          sourceCodePro.variable
         )}
       >
         {children}
