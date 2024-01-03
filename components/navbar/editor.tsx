64677456daf5827868fd0c4add42d7f844b33a8c
Hiroshi Fujita <fujita-h@users.noreply.github.com>
2024-01-03 19:24:59 +0900

    Add EditorNavbar component and refactoring

---
+++ components/navbar/editor.tsx
@@ -0,0 +1,130 @@
+'use client';
+
+import { SITE_NAME } from '@/libs/constants';
+import { Menu, Transition } from '@headlessui/react';
+import clsx from 'clsx';
+import Link from 'next/link';
+import { Fragment } from 'react';
+import { useFormStatus } from 'react-dom';
+
+const userNavigation = [
+  { name: 'プロフィール', href: '/profile' },
+  { name: 'ストック', href: '/stocks' },
+  { name: '下書き', href: '/drafts' },
+  { name: '設定', href: '/settings' },
+];
+
+export function EditorNavbar({
+  formDraftAction,
+  formPublishAction,
+  showAutoSavingMessage,
+}: {
+  formDraftAction: () => void;
+  formPublishAction: () => void;
+  showAutoSavingMessage: boolean;
+}) {
+  return (
+    <div className="border-b border-inset">
+      <div className="max-w-screen-2xl mx-auto px-8">
+        <div className="flex h-14 justify-between">
+          <div className="flex flex-shrink-0 items-center">
+            <Link href="/">
+              <div className="flex items-center">
+                <div className="pt-1">
+                  <span className="text-2xl text-gray-700 font-semibold">{SITE_NAME}</span>
+                </div>
+              </div>
+            </Link>
+          </div>
+          <div className="flex items-center gap-2">
+            <AutoSavingMessage show={showAutoSavingMessage} />
+            <DraftButton onClick={formDraftAction} />
+            <PublishButton onClick={formPublishAction} />
+            {/* Profile dropdown */}
+            <Menu as="div" className="ml-1 relative flex-shrink-0">
+              <div>
+                <Menu.Button className="flex rounded-full bg-white text-sm focus:outline-none hover:ring-gray-300 hover:ring-2 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                  <span className="sr-only">Open user menu</span>
+                  <img src="/api/user/icon" width={32} height={32} className="rounded-full" alt="user-icon" />
+                </Menu.Button>
+              </div>
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+              >
+                <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-2 ring-black ring-opacity-5 focus:outline-none">
+                  {userNavigation.map((item) => (
+                    <Menu.Item key={item.name}>
+                      {({ active }) => (
+                        <Link
+                          href={item.href}
+                          className={clsx(
+                            active ? 'bg-gray-100' : '',
+                            'block px-4 py-2 text-sm font-semibold text-gray-600'
+                          )}
+                        >
+                          {item.name}
+                        </Link>
+                      )}
+                    </Menu.Item>
+                  ))}
+                </Menu.Items>
+              </Transition>
+            </Menu>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function AutoSavingMessage({ show }: { show: boolean }) {
+  return (
+    <Transition
+      show={show}
+      enter="transition ease-out duration-100"
+      enterFrom="transform opacity-0 scale-95"
+      enterTo="transform opacity-100 scale-100"
+      leave="transition ease-in duration-100"
+      leaveFrom="transform opacity-100 scale-100"
+      leaveTo="transform opacity-0 scale-95"
+    >
+      <span className="px-1 text-sm font-medium text-indigo-500 animate-pulse">Auto-Saving...</span>
+    </Transition>
+  );
+}
+
+function DraftButton({ onClick }: { onClick: (e: React.MouseEvent<HTMLButtonElement>) => void }) {
+  const { pending } = useFormStatus();
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      disabled={pending}
+      aria-disabled={pending}
+      className="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-gray-500 hover:bg-gray-400 focus-visible:outline focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-gray-500"
+    >
+      下書きに保存
+    </button>
+  );
+}
+
+function PublishButton({ onClick }: { onClick: (e: React.MouseEvent<HTMLButtonElement>) => void }) {
+  const { pending } = useFormStatus();
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      disabled={pending}
+      aria-disabled={pending}
+      className="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus-visible:outline focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-indigo-500"
+    >
+      公開する
+    </button>
+  );
+}
