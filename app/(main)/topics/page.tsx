ead39d3ddcbb537fa73fa2f3694d2f356a53c000
Hiroshi Fujita <fujita-h@users.noreply.github.com>
2024-01-04 14:09:22 +0900

    Add user role check for topic creation

--- app/(main)/topics/page.tsx
+++ app/(main)/topics/page.tsx
@@ -2,9 +2,11 @@ import { SignInForm } from '@/components/auth/sign-in-form';
 import { Error404, Error500 } from '@/components/error';
 import { TopicBadge } from '@/components/topics/badge';
 import { auth } from '@/libs/auth';
-import { getUserIdFromSession } from '@/libs/auth/utils';
+import { getUserIdFromSession, getRolesFromSession } from '@/libs/auth/utils';
 import { getTopics } from '@/libs/prisma/topic';
 import { AddTopicButton } from './form';
+
+const USER_ROLE_FOR_TOPIC_CREATION = process.env.USER_ROLE_FOR_TOPIC_CREATION || '';
 
 export default async function Page() {
   const session = await auth();
@@ -12,6 +14,10 @@ export default async function Page() {
   if (status === 401) return <SignInForm />;
   if (status === 500) return <Error500 />;
   if (status === 404 || !userId) return <Error404 />;
+
+  // Get user roles from session, and check if the user is allowed to create a topic
+  const roles = await getRolesFromSession(session);
+  const isAllowedToCreateTopic = !USER_ROLE_FOR_TOPIC_CREATION || roles.includes(USER_ROLE_FOR_TOPIC_CREATION);
 
   const topics = await getTopics();
 
@@ -22,9 +28,7 @@ export default async function Page() {
           <div className="text-2xl font-bold">Topics</div>
           <div className="text-gray-600">トピック</div>
         </div>
-        <div>
-          <AddTopicButton />
-        </div>
+        <div>{isAllowedToCreateTopic && <AddTopicButton />}</div>
       </div>
       <div className="mt-6 flex">
         {/* <div className="w-80 min-w-[320px]">left menu</div> */}
