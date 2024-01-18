e7bbe5c14d1c19c30a5b0faeffb2e8fe57b8fe4e
Hiroshi Fujita <fujita-h@users.noreply.github.com>
2024-01-18 23:25:35 +0900

    Update next-auth and @auth/core versions With this update, the sessionCallback argument now depends on the strategy, so we changed it to a type that corresponds to the JWT strategy.

--- libs/auth/callbacks/session.ts
+++ libs/auth/callbacks/session.ts
@@ -1,19 +1,12 @@
-import { AdapterUser } from '@auth/core/adapters';
 import { JWT } from '@auth/core/jwt';
 import { DefaultSession, Session } from 'next-auth';
 
 export const sessionCallback = async ({
   session,
   token,
-  user,
-  newSession,
-  trigger,
 }: {
   session: Session;
-  token: JWT;
-  user: AdapterUser;
-  newSession: any;
-  trigger: 'update';
+  token?: JWT;
 }): Promise<Session | DefaultSession> => {
   // include token in session
   return {
