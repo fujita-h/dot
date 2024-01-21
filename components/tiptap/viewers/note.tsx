18aaea4706458f46e8d9af100b9f2ef0baaf6f24
Hiroshi Fujita <fujita-h@users.noreply.github.com>
2024-01-21 19:53:12 +0900

    Remove CodeBlockExtension CodeBlockExtension is not required if CodeBlockLowlightExtension is present.

--- components/tiptap/viewers/note.tsx
+++ components/tiptap/viewers/note.tsx
@@ -7,7 +7,6 @@ import BlockquoteExtension from '@tiptap/extension-blockquote';
 import BoldExtension from '@tiptap/extension-bold';
 import BulletListExtension from '@tiptap/extension-bullet-list';
 import CodeExtension from '@tiptap/extension-code';
-import CodeBlockExtension from '@tiptap/extension-code-block';
 import CodeBlockLowlightExtension from '@/libs/tiptap/extensions/code-block-lowlight';
 import DocumentExtension from '@tiptap/extension-document';
 import DropcursorExtension from '@tiptap/extension-dropcursor';
@@ -41,7 +40,6 @@ export default function TipTapJsonNoteRenderer({ jsonString }: { jsonString: str
     extensions: [
       BlockquoteExtension,
       BulletListExtension,
-      CodeBlockExtension,
       CodeBlockLowlightExtension,
       DocumentExtension,
       HardBreakExtension,
