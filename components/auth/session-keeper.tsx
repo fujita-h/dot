7d38a732acb77593ece7aa67c001d23fc858755c
Hiroshi Fujita <fujita-h@users.noreply.github.com>
2024-01-21 15:41:24 +0900

    Fix timeout value in SessionKeeper component

--- components/auth/session-keeper.tsx
+++ components/auth/session-keeper.tsx
@@ -2,13 +2,13 @@
 
 import { useInterval } from '@/libs/utils/react';
 
-export function SessionKeeper({ interval = 60, timeout = 5000 }: { interval?: number; timeout?: number }) {
+export function SessionKeeper({ interval = 60, timeout = 5 }: { interval?: number; timeout?: number }) {
   useInterval(
     () => {
       const abortController = new AbortController();
       const timeoutId = setTimeout(() => {
         abortController.abort();
-      }, timeout);
+      }, timeout * 1000);
       fetch('/api/auth/session', { method: 'GET', cache: 'no-cache', signal: abortController.signal })
         .then((data) => data)
         .catch(() => null)
