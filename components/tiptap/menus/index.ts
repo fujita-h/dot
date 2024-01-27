331fb3a1532bae6eaa9b3d3baec1c04d59fa6bfe
Hiroshi Fujita <fujita-h@users.noreply.github.com>
2024-01-27 22:20:45 +0900

    Add #14 link functionality to editor form

--- components/tiptap/menus/index.ts
+++ components/tiptap/menus/index.ts
@@ -1,4 +1,5 @@
 export { BubbleMenuImage } from './bubble-menu-image';
+export { BubbleMenuLink } from './bubble-menu-link';
 export { BubbleMenuTable } from './bubble-menu-table';
 export { BubbleMenuTextSelected } from './bubble-menu-text-selected';
 export { FloatingMenuNewLine } from './floating-menu-new-line';
