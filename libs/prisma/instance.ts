c1350ca814255f797ecc1dd097aa26902a24489f
Hiroshi Fujita <fujita-h@users.noreply.github.com>
2024-02-10 21:17:01 +0900

    Refactor Prisma client initialization

--- libs/prisma/instance.ts
+++ libs/prisma/instance.ts
@@ -1,7 +1,23 @@
 import { PrismaClient } from '@prisma/client';
 
 const prismaClientSingleton = () => {
-  return new PrismaClient();
+  if (process.env.NODE_ENV !== 'production') {
+    // development
+    const prisma = new PrismaClient({
+      log: ['info', 'warn', 'error', { emit: 'event', level: 'query' }],
+    });
+    prisma.$on('query', (e) => {
+      const { timestamp, query, params, duration } = e;
+      console.log('prisma:query', { timestamp, query, params, duration });
+    });
+    return prisma;
+  } else {
+    // production
+    const prisma = new PrismaClient({
+      log: ['info', 'error'],
+    });
+    return prisma;
+  }
 };
 
 declare global {
