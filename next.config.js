5005caa639e44ef7f23e2169d1b79920818c2aba
Hiroshi Fujita <fujita-h@users.noreply.github.com>
2024-02-14 20:09:09 +0900

    Fix frame-ancestors value in Content-Security-Policy header (#34)

--- next.config.js
+++ next.config.js
@@ -32,7 +32,7 @@ const nextConfig = {
           {
             key: 'Content-Security-Policy',
             value:
-              "default-src 'self'; img-src 'self' data:; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; frame-ancestors: 'none';",
+              "default-src 'self'; img-src 'self' data:; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; frame-ancestors 'none';",
           },
           {
             key: 'Strict-Transport-Security',
