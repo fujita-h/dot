2ce617f047ba385f98ad1976fab0d49cfaad6ea5
Hiroshi Fujita <fujita-h@users.noreply.github.com>
2024-02-18 23:11:40 +0900

    Update menu button labels

--- app/(main)/notes/[noteId]/form.tsx
+++ app/(main)/notes/[noteId]/form.tsx
@@ -91,7 +91,7 @@ export function OtherMenuButton({ userId, note, className }: { userId: string; n
                       className="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
                       aria-hidden="true"
                     />
-                    Edit
+                    編集する
                   </a>
                 )}
               </Menu.Item>
@@ -147,7 +147,7 @@ export function OtherMenuButton({ userId, note, className }: { userId: string; n
                     className="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
                     aria-hidden="true"
                   />
-                  Duplicate
+                  複製する
                 </a>
               )}
             </Menu.Item>
@@ -165,7 +165,7 @@ export function OtherMenuButton({ userId, note, className }: { userId: string; n
                     onClick={() => setOpenDeleteModal(true)}
                   >
                     <TrashIcon className="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500" aria-hidden="true" />
-                    Delete
+                    削除する
                   </button>
                 )}
               </Menu.Item>
