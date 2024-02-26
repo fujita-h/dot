973c8ae498019a2d1453480c72d8b2ff24494a5a
Hiroshi Fujita <fujita-h@users.noreply.github.com>
2024-02-27 00:21:44 +0900

    Add DeleteGroup function and DeleteForm component (#43)

--- app/(main)/groups/0/[groupId]/settings/general/form.tsx
+++ app/(main)/groups/0/[groupId]/settings/general/form.tsx
@@ -1,11 +1,13 @@
 'use client';
 
-import { useFormStatus, useFormState } from 'react-dom';
-import { ActionState, UpdateGroupAction } from './action';
-import { useEffect, useRef, useState } from 'react';
-import { CameraIcon } from '@heroicons/react/24/outline';
+import { Dialog, Transition } from '@headlessui/react';
+import { CameraIcon, ExclamationTriangleIcon, XMarkIcon } from '@heroicons/react/24/outline';
+import { useRouter } from 'next/navigation';
+import { Fragment, useEffect, useRef, useState } from 'react';
+import { useFormState, useFormStatus } from 'react-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { ActionState, DeleteGroup, UpdateGroupAction } from './action';
 
 const initialActionState: ActionState = {
   status: null,
@@ -260,5 +262,137 @@ export function Form({ group }: { group: Group }) {
         <SubmitButton />
       </div>
     </form>
+  );
+}
+
+export function DeleteForm({ group }: { group: Group }) {
+  const router = useRouter();
+  const [open, setOpen] = useState(false);
+  const [confirmText, setConfirmText] = useState('');
+
+  useEffect(() => {
+    if (open) {
+      setConfirmText('');
+    }
+  }, [open]);
+
+  return (
+    <div>
+      <div className="flex items-center p-4 sm:p-6">
+        <div className="flex-1">
+          <div className="text-base font-medium leading-6 text-gray-900">グループを削除する</div>
+          <p className="text-sm font-normal text-gray-500">
+            グループを削除すると、グループの全てのデータが削除されます。この操作は取り消せません。
+            <br />
+            記事が投稿されている場合は、すべての記事を削除してからでないとグループを削除できません。
+          </p>
+        </div>
+        <div className="flex-none">
+          <button
+            type="button"
+            className="rounded-md bg-gray-50 ring-1 ring-gray-300 px-3 py-2 text-sm font-semibold text-red-500 shadow-sm hover:bg-red-500 hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
+            onClick={() => setOpen(true)}
+          >
+            グループを削除する
+          </button>
+        </div>
+      </div>
+      <Transition.Root show={open} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={setOpen}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                enterTo="opacity-100 translate-y-0 sm:scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              >
+                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg lg:max-w-2xl sm:p-6">
+                  <div className="absolute right-0 top-0 hidden pr-4 pt-4 sm:block">
+                    <button
+                      type="button"
+                      className="rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                      onClick={() => setOpen(false)}
+                    >
+                      <span className="sr-only">Close</span>
+                      <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                    </button>
+                  </div>
+                  <div className="sm:flex sm:items-start">
+                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                      <ExclamationTriangleIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
+                    </div>
+                    <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                      <Dialog.Title as="h3" className="mt-2 text-base font-semibold leading-6 text-gray-900">
+                        グループ {group.name} を削除する
+                      </Dialog.Title>
+                      <div className="mt-2">
+                        <p className="text-sm text-gray-500">
+                          グループハンドル
+                          <span className="text-gray-900 font-medium mx-1">{group.handle}</span>
+                          を削除しようとしています。
+                          グループを削除するには、ハンドル名を入力してください。この操作は取り消せません。
+                        </p>
+                      </div>
+                      <div className="mt-2">
+                        <input
+                          type="text"
+                          className="mt-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-red-700 sm:text-sm sm:leading-6"
+                          value={confirmText}
+                          onChange={(e) => setConfirmText(e.target.value)}
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
+                    <button
+                      type="button"
+                      className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto disabled:bg-red-600/30  disabled:cursor-not-allowed"
+                      disabled={confirmText !== group.handle}
+                      onClick={async () => {
+                        try {
+                          await DeleteGroup(group.id, confirmText);
+                          router.push('/groups');
+                        } catch (e: unknown) {
+                          if (e instanceof Error) {
+                            alert('エラー: グループの削除に失敗しました。\n' + e.message);
+                          } else {
+                            alert('エラー: グループの削除に失敗しました。');
+                          }
+                        }
+                      }}
+                    >
+                      グループを削除する
+                    </button>
+                    <button
+                      type="button"
+                      className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                      onClick={() => setOpen(false)}
+                    >
+                      キャンセル
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition.Root>
+    </div>
   );
 }
