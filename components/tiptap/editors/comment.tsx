3d573831734fc0be6ab986a10b9ba355fe3fac4d
Hiroshi Fujita <fujita-h@users.noreply.github.com>
2024-03-07 01:16:24 +0900

    Refactor comment editor button styles (#49)

--- components/tiptap/editors/comment.tsx
+++ components/tiptap/editors/comment.tsx
@@ -134,10 +134,10 @@ export default function CommentEditor({
             <EditorContent editor={editor} />
           </div>
           {isEdit ? (
-            <div className="mt-3 flex justify-end">
+            <div className="mt-3 flex justify-end gap-2">
               <button
                 type="button"
-                className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="inline-flex items-center rounded-md px-3 py-2 text-sm font-semibold text-gray-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-500"
                 onClick={async () => {
                   cancelAction?.();
                 }}
