196f41d12d1e7711f128b691bbea02f3b64c0f40
Hiroshi Fujita <fujita-h@users.noreply.github.com>
2024-03-11 01:28:55 +0900

    Refactor setFollow function (#53)

--- app/(main)/topics/[handle]/action.ts
+++ app/(main)/topics/[handle]/action.ts
@@ -4,29 +4,34 @@ import { getSessionUser } from '@/libs/auth/utils';
 import prisma from '@/libs/prisma/instance';
 import { revalidatePath } from 'next/cache';
 
-export async function setFollow(topicId: string, follow: boolean): Promise<boolean> {
+export async function setFollow(topicId: string, follow: boolean) {
   const user = await getSessionUser();
-  if (!user || !user.id) throw new Error('Unauthorized');
+  if (!user || !user.id) {
+    return { error: 'Unauthorized' };
+  }
   const userId = user.id;
 
   const topic = await prisma.topic.findUnique({ where: { id: topicId } });
-  if (!topic) throw new Error('Group not found');
+  if (!topic) {
+    return { error: 'Topic not found' };
+  }
 
-  const followed = await prisma.followTopic.findUnique({ where: { userId_topicId: { userId, topicId } } });
+  const followed = await prisma.followTopic.findUnique({
+    where: { userId_topicId: { userId, topicId } },
+    select: { userId: true, topicId: true },
+  });
   if (follow && !followed) {
     const f = await prisma.followTopic
-      .create({ data: { userId, topicId } })
-      .then((data) => Boolean(data))
-      .catch((e) => false);
+      .create({ data: { userId, topicId }, select: { userId: true, topicId: true } })
+      .catch((e) => null);
     revalidatePath(`/topics/${topic.handle}`);
     return f;
   } else if (!follow && followed) {
     const f = await prisma.followTopic
-      .delete({ where: { userId_topicId: { userId, topicId } } })
-      .then((data) => Boolean(data))
-      .catch((e) => false);
+      .delete({ where: { userId_topicId: { userId, topicId } }, select: { userId: true, topicId: true } })
+      .catch((e) => null);
     revalidatePath(`/topics/${topic.handle}`);
     return f;
   }
-  return Boolean(followed);
+  return followed;
 }
