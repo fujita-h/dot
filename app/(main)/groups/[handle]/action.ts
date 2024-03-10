cd971fe06b606d814113a079e5226ff5d3cd833c
Hiroshi Fujita <fujita-h@users.noreply.github.com>
2024-03-10 23:43:07 +0900

    Refactor group action functions (#53)

--- app/(main)/groups/[handle]/action.ts
+++ app/(main)/groups/[handle]/action.ts
@@ -5,68 +5,90 @@ import prisma from '@/libs/prisma/instance';
 import { GroupType, MembershipRole } from '@prisma/client';
 import { revalidatePath } from 'next/cache';
 
-export async function setFollow(groupId: string, follow: boolean): Promise<boolean> {
+export async function setFollow(groupId: string, follow: boolean) {
   const user = await getSessionUser();
-  if (!user || !user.id) throw new Error('Unauthorized');
+  if (!user || !user.id) {
+    return { error: 'Unauthorized' };
+  }
   const userId = user.id;
 
   const group = await prisma.group.findUnique({ where: { id: groupId }, include: { Members: true } });
-  if (!group) throw new Error('Group not found');
+  if (!group) {
+    return { error: 'Group not found' };
+  }
   if (group.type === GroupType.PRIVATE && !group.Members.find((m) => m.userId === userId)) {
-    throw new Error('Unauthorized');
+    return { error: 'Unauthorized' };
   }
 
-  const followed = await prisma.followGroup.findUnique({ where: { userId_groupId: { userId, groupId } } });
+  const followed = await prisma.followGroup.findUnique({
+    where: { userId_groupId: { userId, groupId } },
+    select: { userId: true, groupId: true },
+  });
   if (follow && !followed) {
     const f = await prisma.followGroup
-      .create({ data: { userId, groupId } })
-      .then((data) => Boolean(data))
+      .create({
+        data: { userId, groupId },
+        select: { userId: true, groupId: true },
+      })
       .catch((e) => false);
     revalidatePath(`/groups/${group.handle}`);
     return f;
   } else if (!follow && followed) {
     const f = await prisma.followGroup
-      .delete({ where: { userId_groupId: { userId, groupId } } })
-      .then((data) => Boolean(data))
+      .delete({
+        where: { userId_groupId: { userId, groupId } },
+        select: { userId: true, groupId: true },
+      })
       .catch((e) => false);
     revalidatePath(`/groups/${group.handle}`);
     return f;
   }
-  return Boolean(followed);
+  return followed;
 }
 
-export async function joinCommunity(groupId: string, join: boolean): Promise<boolean> {
+export async function joinCommunity(groupId: string, join: boolean) {
   const user = await getSessionUser();
-  if (!user || !user.id) throw new Error('Unauthorized');
+  if (!user || !user.id) {
+    return { error: 'Unauthorized' };
+  }
   const userId = user.id;
 
-  const group = await prisma.group.findUnique({ where: { id: groupId }, include: { Members: true } });
-  if (!group) throw new Error('Group not found');
+  const group = await prisma.group.findUnique({
+    where: { id: groupId },
+    include: { Members: { select: { userId: true, groupId: true, role: true } } },
+  });
+  if (!group) {
+    return { error: 'Group not found' };
+  }
   if (group.type !== GroupType.COMMUNITY) {
-    throw new Error('This group is not a community');
+    return { error: 'This group is not a community' };
   }
 
   const myMembership = group.Members.find((m) => m.userId === userId);
   if (myMembership && myMembership.role === 'ADMIN') {
-    throw new Error('Admins cannot join/leave the group using this method');
+    return { error: 'Admins cannot join/leave the group using this method' };
   }
 
   if (join && !myMembership) {
     const f = await prisma.membership
-      .create({ data: { userId, groupId, role: MembershipRole.CONTRIBUTOR } })
-      .then((data) => Boolean(data))
+      .create({
+        data: { userId, groupId, role: MembershipRole.CONTRIBUTOR },
+        select: { userId: true, groupId: true, role: true },
+      })
       .catch((e) => false);
     revalidatePath(`/groups/${group.handle}`);
     return f;
   } else if (!join && myMembership) {
     const f = await prisma.membership
-      .delete({ where: { userId_groupId: { userId, groupId } } })
-      .then((data) => Boolean(data))
+      .delete({
+        where: { userId_groupId: { userId, groupId } },
+        select: { userId: true, groupId: true, role: true },
+      })
       .catch((e) => false);
     revalidatePath(`/groups/${group.handle}`);
     return f;
   } else {
     revalidatePath(`/groups/${group.handle}`);
-    return Boolean(myMembership);
+    return myMembership;
   }
 }
