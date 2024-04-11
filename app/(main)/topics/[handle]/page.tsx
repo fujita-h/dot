d98175f2d39ea8714a5dad7aba41c5a6df676a08
Hiroshi Fujita <fujita-h@users.noreply.github.com>
2024-04-11 21:46:49 +0900

    Fix topic page layout and styling

--- app/(main)/topics/[handle]/page.tsx
+++ app/(main)/topics/[handle]/page.tsx
@@ -36,20 +36,18 @@ export default async function Page({ params, searchParams }: Props) {
   return (
     <div className="md:flex md:gap-2 md:m-2">
       <div className="md:w-80 px-2">
-        <div className="relative p-4 bg-white rounded-md">
+        <div className="relative px-4 py-6 bg-white rounded-md">
           <div className="absolute top-0 right-0 mt-1 mr-1">
             <OtherMenuButton id={topic.id} />
           </div>
-          <div className="flex justify-center">
-            <div className="text-center">
-              <img
-                src={`/api/topics/${topic.id}/icon`}
-                className="w-16 h-16 rounded-md bg-white aspect-square object-cover"
-                alt="topic icon"
-              />
-              <div className="text-lg font-semibold text-gray-800">{topic.name}</div>
-            </div>
+          <div className="mt-2 flex justify-center">
+            <img
+              src={`/api/topics/${topic.id}/icon`}
+              className="w-16 h-16 rounded-md bg-white aspect-square object-cover"
+              alt="topic icon"
+            />
           </div>
+          <div className="mt-2 text-center text-lg font-semibold text-gray-800">{topic.name}</div>
           <div className="mt-6 flex justify-center">
             <FollowToggleButton id={topic.id} isFollowing={isFollowing} />
           </div>
